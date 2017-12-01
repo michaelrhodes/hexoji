8326ead870614ee0df22ddc7c1e691d5affef9ea
michaelrhodes <spam@michaelrhod.es>
2017-12-01 14:07:46 +1100

    Make decode function more robust

--- decode.js
+++ decode.js
@@ -1,10 +1,16 @@
 var dec = require('punycode2/ucs2/decode')
+var hex = require('./is-hex')
 
 module.exports = decode
 
 function decode (str) {
-  return str && dec(str).map(function (co) {
-    var str = co.toString(16)
-    return str[str.length - 1]
-  }).join('') || null
+  var base, val = str && dec(str).map(function (co, i) {
+    var v = co.toString(16), l = v.length
+    if (!i) base = v[l - 2]
+    return v.toUpperCase().slice(0, -2) === '1F6' &&
+      v[l - 2] === base ?
+      v[l - 1] : 'X'
+  }).join('')
+
+  return val && hex.test(val) ? val : null
 }
--- encode.js
+++ encode.js
@@ -1,13 +1,13 @@
 var enc = require('punycode2/ucs2/encode')
-var valid = /^[0-9A-F]+$/
+var hex = require('./is-hex')
 
 module.exports = encode
 
 function encode (str, base) {
   str = str && str.toUpperCase()
   base = '1F6' + (base || 0)
-  return str && valid.test(str) &&
+  return str && hex.test(str) ?
     enc(str.split('').map(function (ch) {
       return parseInt(base + ch, 16)
-    })) || null
+    })) : null
 }
