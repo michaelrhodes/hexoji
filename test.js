73c663b0884ff0430416e9ae040c3eb1e95890e7
michaelrhodes <spam@michaelrhod.es>
2017-12-01 14:14:27 +1100

    Add basic tests

---
+++ test.js
@@ -0,0 +1,14 @@
+var test = require('tape')
+var sha1 = require('simple-sha1')
+var encode = require('./encode')
+var decode = require('./decode')
+
+test('it works', function (t) {
+  var input = sha1.sync('hexoticon')
+  t.equal(decode(encode(input)), input)
+  t.equal(decode(encode(input, 1)), input)
+  t.equal(decode(encode(input, 2)), input)
+  t.equal(decode(encode(input, 3)), input)
+  t.equal(decode(encode(input, 4)), input)
+  t.end()
+})
